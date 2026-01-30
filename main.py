6c2c9e0d18a3344d672a9ecacaec96cfa31b03ff
manimaran <manimaranh9965@gmail.com>
2026-01-30 21:58:50 +0530

    second day

--- main.py
+++ main.py
@@ -1,24 +1,26 @@
 import pandas as pd
-from sklearn.preprocessing import StandardScaler
-from sklearn.preprocessing import PowerTransformer
+from sklearn.model_selection import train_test_split
 from matplotlib import pyplot as plt
+from sklearn.preprocessing import PowerTransformer
 import seaborn as sns
 
 mall=pd.read_csv("Mall_Customers.csv")
 
-mall.columns=(mall.columns.str.strip())
-
-mall=mall.drop(columns="CustomerID")
-x=mall[["Annual Income (k$)","Spending Score (1-100)"]]
-
-mall["Gender"]=mall["Gender"].map({"Male":0,"Female":1})
-#print(mall["Gender"])
 
 
-#plt.subplot(1,2,1)
-#sns.histplot(mall["Spending Score (1-100)"])
+mall["Gender"]=mall["Gender"].map({"Male":1,"Female":0})
 
-#plt.subplot(1,2,2)
-#sns.histplot(mall["Annual Income (k$)"])
-#plt.show()
+mall=mall.drop(columns=["CustomerID"])
+#print(mall.head(10))
 
+x=mall[["Gender","Age","Annual Income (k$)","Spending Score (1-100)"]]
+ 
+
+y=mall[["Annual Income (k$)","Spending Score (1-100)"]]
+
+pt=PowerTransformer(method="yeo-johnson")
+x_train=pt.fit_transform(y)
+x_test=pt.transform(y)
+
+
+print(x_train)
